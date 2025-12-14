274f1915200a8c3f234f436c02fa77ed5f50078d
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2025-12-14 12:59:46 +0100

    Update AttestationRegistry.sol

--- contracts/AttestationRegistry.sol
+++ contracts/AttestationRegistry.sol
@@ -14,6 +14,11 @@ contract AttestationRegistry is Ownable {
 
     event Attested(address indexed user, string description);
 
+    // Add this constructor to resolve the error
+    constructor(address initialOwner) Ownable(initialOwner) {
+        // The contract owner will be set to the initialOwner address provided at deployment
+    }
+
     function attest(address user, string calldata description) external onlyOwner {
         attestations[user].push(
             Attestation(msg.sender, description, block.timestamp)
