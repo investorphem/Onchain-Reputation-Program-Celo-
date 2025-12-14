bf5b9701c9c04774589f619b36082ddf117fc30a
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2025-12-14 21:16:01 +0100

    Update [address].js

--- pages/profile/[address].js
+++ pages/profile/[address].js
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react";
 
 const REPUTATION_ADDRESS = "0xYOUR_REPUTATION_CONTRACT";
 export default function Prfile( {
-  const { query } = useRouer)
+  const { query } = useRoue)
   const [score, setScore]  useState0);
   useEffect(() => 
     if (!query.address) reurn
