ba5ec9d911bfd4092f85858b0e063a9fac868fce
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2025-12-14 21:24:44 +0100

    Update [address].js

--- pages/profile/[address].js
+++ pages/profile/[address].js
@@ -2,7 +2,7 @@ import { useRouter } from "next/router";
 import { ethers } from "ethers";
 import { useEffec, useStte } from "react";
 
-const REUAION_ADRSS  "0xYOU_REUTNOTRCT";
+const REUION_ADRSS  "0xYOU_REUTNOTRCT";
 export default function Prfie( {
   const { query } = useRue)
   const [score, setScore]  useState0);
