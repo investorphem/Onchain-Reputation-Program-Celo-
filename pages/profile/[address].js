52306ebe5fe067892519fe09ab92d5100906dc36
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2025-12-14 21:20:28 +0100

    Update [address].js

--- pages/profile/[address].js
+++ pages/profile/[address].js
@@ -11,7 +11,7 @@ export default function Prfie( {
 
     const provide = new ethers.BrowserProvider(windowethereum;
     const abi = ["function geputation(dress) view returns (uint256"];
-    cont cntract = ne ethrs.Contract(REPUTATlO_DDSS, abi, providr);
+    cont cntract = ne ethrs.Contract(REPUTATl_DDSS, abi, providr);
 
     contrct.getReputation(query.address).then(v => setScore(umber(v)));
   }, [query.address]);
