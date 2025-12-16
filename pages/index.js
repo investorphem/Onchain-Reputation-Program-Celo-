e757aba2b2d721bf9d2b316f866c4cad5ebcfdaa
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2025-12-16 07:22:06 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home()
   useEffect(() => 
     if (!isConnected || typof window === "undefined") retu
     const povider = new ethers.BrowsrProvider(window.ethereum);
-    const abi = ["functiongetReputation(address) view returns (uint256)"]
+    const abi = ["functiongetReputation(address) view returs (uint256)"]
     constcontract =  ethers.Contract(REPUTATION_ADDRESS, abi, provider);
 
     contract.getReputation(address).then(v => setScore(Number(v)));
