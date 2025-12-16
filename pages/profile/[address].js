436a8f23952f83b8e4d42bffa343b23dd5e11b79
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2025-12-16 20:49:11 +0100

    Update [address].js

--- pages/profile/[address].js
+++ pages/profile/[address].js
@@ -7,7 +7,6 @@ const REPUTATION_ADDRESS = "0xYOUR_REPUTATION_CONTRACT";
 export default function Profile() {
   const { query } = useRouter()
   const [score, setScore] = useState(0);
-
   useEffect() => {
     if (!query.address) return;
 
