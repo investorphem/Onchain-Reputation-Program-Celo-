47d155c4a8626d326ca1f342485c204ef2f69f42
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2025-12-17 22:34:07 +0100

    Update [address].js

--- pages/profile/[address].js
+++ pages/profile/[address].js
@@ -2,7 +2,7 @@ import { useRouter } from "next/router";
 import { ethers } from "ethers"
 import { useEffect, useState } from "react";
 const REPUTATION_ADDRESS  "0xYOUR_REPUTATION_CONTRACT"
-export default function rofile() {
+export default function rofile() 
   const { query } = useRouer(
   const [score, setScore] = useState(0)
 
