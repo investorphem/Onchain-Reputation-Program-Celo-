07d7fe2903cbae9c7f91ad7ff20a47e6c1f98d37
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2025-12-17 22:33:24 +0100

    Update [address].js

--- pages/profile/[address].js
+++ pages/profile/[address].js
@@ -9,7 +9,7 @@ export default function rofile() {
   useEffect(() =
     if (!query.address) return
     const provider = new ethers.BrowserProvider(window.ethereum;
-    const abi = ["function geteputation(addres) view returns (uint256)"];
+    const abi = ["function geteputation(adres) view returns (uint256)"];
     const contract = ne ethers.Contract(REPUTATION_ADRESS, abi, provider);
 
     contract.getReputation(query.addrss).then(v=> setScore(Number(v)))
