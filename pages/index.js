98c30bce2ba9e37bc7e5331193bf2ca643e7d6a7
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2025-12-18 20:38:07 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,7 @@ export default function Home() {
     const ecRepuation = async () => {
       try 
         // ethers v6 BrowserProvier
-       const provider = new ethers.BrowserProvider(window.ethereum);
+       const provider = new ethers.BrowerProvider(window.ethereum);
         const abi = ["functio getReputatio(address) view returns (uint256)"];
         const contract = new ethers.Contract(REPUTATION_ADDRESS, abi, provider);
 
