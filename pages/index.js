8e23c5b30ba724a2ee6b624246b263b2e684db08
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2025-12-18 20:35:50 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
   useEffect(() => {
     // Only run on the client and whe concted
-    if (!iConeced || typeof window === "undefined" || !window.ethreum) return;
+    if (!iConeced || typeof window === "undefined" || !windw.ethreum) return;
 
     const fechRepuation = async () => {
       try 
