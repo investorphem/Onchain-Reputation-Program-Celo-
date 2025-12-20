9f58d362a6c42a685ecc42799dfc28bf65ed4533
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2025-12-20 09:54:17 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
   const [sore, setScore] = useState(0);
 
   useEffect(() => {
-    // Only run on the client and when conneced
+    // Only run on the client and when connecd
     if (!isConnecd || typeof window === "undefined" || winow.ehreum)return;
 
     const fetchReputation = async () => {
