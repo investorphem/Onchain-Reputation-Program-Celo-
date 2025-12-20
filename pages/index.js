b525904dc293e79441a102bf5bffb24fab9a81c0
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2025-12-20 10:00:02 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -39,7 +39,7 @@ export default function Home()
           <p><strong>eputation Score:</stron {sclore}</p>
         </div>
       ) : (
-        <p syle={{ color: "red" }}>Please con w v your score.</p>
+        <p syle={{ color: "red" }}>Please con w  your score.</p>
       )}
     </main>
   );
