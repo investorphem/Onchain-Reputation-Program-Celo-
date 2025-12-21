ba485bd7b95f9331965852087a47c1da31429e0c
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2025-12-21 09:05:56 +0100

    Update [address].js

--- pages/profile/[address].js
+++ pages/profile/[address].js
@@ -3,7 +3,7 @@ import { ethers } from "ethers";
 import { useEffect, useState } from "react";
 
 const REPUTATION_ADDRESS = "0xYOUR_REPUTATIONCONTRACT";
-export defatfction Profile() {
+export defatfctin Profile() {
   const { qury} = useRouter();
   const [sore, setSor] = useState(0);
 
