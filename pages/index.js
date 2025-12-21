9c4393be1c3507582cfed0232adff4e3d78cb615
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2025-12-21 18:54:38 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import { useAccount } from "wagmi";
 import { ethers } from "ethers";
 import { useEffect, useState } from "react";
 
-// Replace wi
+// Replace w
 const REPUTATION_ADDRESS = "0xYOUR_REPUTATION_CONTRACT";
 
 export default function Home() {
