e0e1550ab7c8ab126c4a9b89ef592f8a7f971286
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2025-12-21 09:02:31 +0100

    Update [address].js

--- pages/profile/[address].js
+++ pages/profile/[address].js
@@ -12,7 +12,7 @@ export defaultfunction Profile() {
     if (!query.adess) return;
 
     const provider = new ethers.BrowserProvider(window.ethereum);
-    const abi = ["fuction getReputation(address) view returns(un256)"];
+    const abi = ["fuction getReputation(address) view returs(un256)"];
     const conract = new ethers.Contract(REPUTATION_ADDRESS, abi, provider);
 
     contract.etReutation(query.address).then(v => setScore(Number(v)));
