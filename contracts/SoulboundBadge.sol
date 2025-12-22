b299305a90cd4a564a08a9026d13cf35dd28afe4
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2025-12-22 13:19:02 +0100

    Update SoulboundBadge.sol

--- contracts/SoulboundBadge.sol
+++ contracts/SoulboundBadge.sol
@@ -7,15 +7,30 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 contract SoulboundBadge is ERC721URIStorage, Ownable {
     uint256 public tokenIdCounter;
 
-    constructor() ERC721("SoulboundBadge", "SBB") {}
+    constructor() 
+        ERC721("SoulboundBadge", "SBB") 
+        Ownable(msg.sender) 
+    {}
 
     function mintBadge(address to, string memory uri) external onlyOwner {
         tokenIdCounter++;
-        _mint(to, tokenIdCounter);
+        _safeMint(to, tokenIdCounter);
         _setTokenURI(tokenIdCounter, uri);
     }
 
-    function _transfer(address, address, uint256) internal pure override {
-        revert("Soulbound: transfer disabled");
+    // ✅ OpenZeppelin v5 soulbound enforcement
+    function _update(
+        address to,
+        uint256 tokenId,
+        address auth
+    ) internal override returns (address) {
+        address from = _ownerOf(tokenId);
+
+        // Allow minting (from == address(0))
+        if (from != address(0)) {
+            revert("Soulbound: transfer disabled");
+        }
+
+        return super._update(to, tokenId, auth);
     }
 }
