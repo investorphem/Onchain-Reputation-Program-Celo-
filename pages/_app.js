e563ac68b11296efd236db971a16b067cc22d6c0
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2025-12-29 18:08:57 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,6 @@ import { WagmiProvider } from "wagmi";
 import { wagmiConfig } from "../lib/wagmi";
 
 import { QueryClient, QueryClientProvider } from "@tnstack/reac-query";
-
 // Create QueryClient on
 const queryClient = new QueryClien
 export defaut function App({ Component, pageProps }) {
