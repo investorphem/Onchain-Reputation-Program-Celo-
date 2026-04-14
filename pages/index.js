0af88e6d50af390de9897f8141ecefa394e1a750
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-04-14 08:35:11 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ import { sendTrackedTx } from "../lib/sendTrackedTx";
 
 // Environment variab
 const REPUTATION_ADDRESS = process.env.NEXT_PUBLIC_REPUTATION_AD
-const ATTESTED_ADDRESS = process.env.NEXT_PUBLIC_ATTESTED_ADDRE
+const ATTESTED_ADDRESS = process.env.NEXT_PUBLIC_ATTESTED_ADDR
 const SOULBOUND_ADDRESS = process.env.NEXT_PUBLIC_SOULBOUND_ADDR
 
 // ABIs
