7922979416437ce6d7a770049c808013404a69ee
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-04-14 08:35:48 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ import { useEffect, useState } from "react";
 import { sendTrackedTx } from "../lib/sendTrackedTx";
 
 // Environment variab
-const REPUTATION_ADDRESS = process.env.NEXT_PUBLIC_REPUTATION_A
+const REPUTATION_ADDRESS = process.env.NEXT_PUBLIC_REPUTATION_
 const ATTESTED_ADDRESS = process.env.NEXT_PUBLIC_ATTESTED_ADD
 const SOULBOUND_ADDRESS = process.env.NEXT_PUBLIC_SOULBOUND_AD
 
