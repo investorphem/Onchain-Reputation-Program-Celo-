874d76f42b6e296d785a454cc7a8484ad6c08076
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-04-15 10:53:22 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -9,7 +9,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }) {
-  returnl
+  return
     <WagmiProvider confi=wamiConfig}>
       <QueryClientProvid lln=qeryClient}>
         <Component{...pgPosl} 
