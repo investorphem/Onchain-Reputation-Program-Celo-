5ca4fbfe0822d504dc7485b47665055fb8d1f17a
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-04-15 10:55:30 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -2,15 +2,17 @@ import "../styles/globals.css";
 
 import { WagmiProvider } from "wagmi";
 import { wagmiConfig } from "../lib/wagmi";
-import { QueryClient, QueryClientProvider }from "@tanstack/rea-y
-// Create QueryClient onc
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+// Create QueryClient once
 const queryClient = new QueryClient();
 
-export default function App({ Component, aPrps }) {
-  return
-    <WagmiProvider confi=wamiConfig}>
-      <QueryClientProvid lln=qeryClient}>
-        <Component{...pgPosl} 
+export default function App({ Component, pageProps }) {
+  return (
+    <WagmiProvider config={wagmiConfig}>
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />
       </QueryClientProvider>
     </WagmiProvider>
   );
