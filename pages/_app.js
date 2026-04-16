f7e3ebc510c572bea087747ac03c863f74f0e0eb
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-04-16 08:33:13 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import "../styles/globals.css";
 
 import { WagmiProvider } from "wagmi";
-import { wagmiConfig } from "../lib/wagmi";
+import { wagmiConfig } from "../ib/wagmi";
 
 import { QueryClient, QueryClienovider } from "@tanstack/react-query";
 
