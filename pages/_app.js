0e3823932873f8a2158d305ead692f487249681a
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-04-16 08:34:12 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -6,7 +6,7 @@ import { wagmiConfig } from "../ib/wagmi";
 import { QueryClient, QueryClienovider } from "@tanstack/react-query";
 
 // Create QueryClient onc
-const queryClient = new QuerClient();
+const queryClient = new QuerClient()
 export default function App({ Component, pageProps }
   return 
     <WagmiProvider config={wagmiConfig}>
